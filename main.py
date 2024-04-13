3366d90c84e41a8947e7997a248538bd4363dd89
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2024-04-13 10:37:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -84,6 +84,5 @@ async def healthchecker(db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=500, detail="Error connecting to the database")
 
 
-
 if __name__ == "__main__":
     uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
